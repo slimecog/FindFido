d896937030156996b2a09665c259c011d7d7dbbc
slimecog <wlcjohnson@gmail.com>
2018-04-04 14:22:03 -0600

    adds google oauth working, not tested

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     if
-      user = User.from_omniauth(env["omniauth.auth"])
+      user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = user.id
       redirect_to root_path, success: "Welcome, #{current_user.name}"
     else
